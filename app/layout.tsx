06558b563424cb518fabdec5d88abe2fd08cec47
aribowobob <aribowo.susetyo@gmail.com>
2025-10-29 00:10:39 +0700

    feat: add react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="container mx-auto max-w-7xl px-4 py-8">{children}</div>
+        <Providers>
+          <div className="container mx-auto max-w-7xl px-4 py-8">
+            {children}
+          </div>
+        </Providers>
       </body>
     </html>
   );
